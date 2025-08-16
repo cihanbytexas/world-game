c40da9924da99e4868d45db7bd088e2fe1f0a847
cihanbytexas <138530537+cihanbytexas@users.noreply.github.com>
2025-08-16 17:05:40 +0300

    Create kelimeler.js

---
+++ kelimeler.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "masa", "kalem", "araba", "bilgisayar", "kitap", "telefon", "çiçek", "çorap", "defter", "koltuk",
+  "elbise", "lamba", "güneş", "ay", "yıldız", "bulut", "yağmur", "kar", "deniz", "nehir"
+];
